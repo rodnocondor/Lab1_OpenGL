6871198fb394a911a8aac40562a53ef149a64ccb
rodnocondor <condormail@yandex.ru>
2025-02-23 20:39:28 +0800

    first commit

---
+++ First_laba/First_laba.cpp
@@ -0,0 +1,64 @@
+﻿// Firstlab.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#define GLEW_DLL
+#define GLFW_DLL
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include <cstdio>
+
+int main()
+{
+	printf("hello world!\n");
+
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window;
+
+	window = glfwCreateWindow(512, 512, "Triangle", NULL, NULL);
+
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+
+	glewInit();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.2, 0.3, 0.4, 1);
+
+		glColor3f(0.5, 0.5, 0.0);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(0, 0.5);
+		glVertex2f(-0.5, -0.5);
+		glVertex2f(0.5, -0.5);
+		glEnd();
+		
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+
+	glfwTerminate();
+}
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
