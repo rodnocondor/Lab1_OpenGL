340769b99d4e31c10bb6a7e8dc8cbbd797ab4323
rodnocondor <condormail@yandex.ru>
2025-04-06 21:28:29 +0800

    Glm_camera

--- First_laba/First_laba.cpp
+++ First_laba/First_laba.cpp
@@ -3,6 +3,9 @@
 
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
+#include "glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 #include <iostream>
 #include <cmath>
 #include <string>
@@ -18,7 +21,74 @@ GLfloat points[] =
 };
 
 GLuint index[] = { 0, 1, 2 };
+const unsigned int WIN_WIDTH = 1024;
+const unsigned int WIN_HEIGHT = 768;
 
+float	lastx = WIN_WIDTH / 2, lasty = WIN_HEIGHT / 2;
+
+GLfloat pitch = -21.0f, yaw = -97.5f;
+glm::vec3 cameraPos = glm::vec3(0.0, 0.0, 5.0);
+glm::vec3 cameraFront = glm::vec3(0.0, 0.0, -1.0);
+glm::vec3 cameraUp = glm::vec3(0.0, 1.0, 0.0);
+bool firstmouse = true;
+
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn) 
+{
+
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+	float xoffset = xpos - lastx;
+	float yoffset = lasty - ypos;
+
+	lastx = xpos;
+	lasty = ypos;
+
+	float sensitivity = 0.08f;
+
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	cameraFront = glm::normalize(front);
+
+
+}
+
+void processInput(GLFWwindow* window) 
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	float cameraSpeed = 0.005;
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		cameraPos += cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		cameraPos -= cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
+
+
+
+void settingMat4(GLuint ID, const char* name, glm::mat4 type) 
+{
+	glUniformMatrix4fv(glGetUniformLocation(ID, name), 1, GL_FALSE, &type[0][0]);
+}
 
 int main()
 {
@@ -32,7 +102,7 @@ int main()
 
 	GLFWwindow* window;
 
-	window = glfwCreateWindow(512, 512, "Triangle", NULL, NULL);
+	window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "Triangle", NULL, NULL);
 
 	glfwMakeContextCurrent(window);
 
@@ -61,8 +131,11 @@ int main()
 
 	GLuint shader_program = load_shader_program("assets\\shader\\vert_shader.vs", "assets\\shader\\frag_shader.fs");
 
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	while (!glfwWindowShouldClose(window))
 	{
+		glfwSetCursorPosCallback(window, mouse_callback);
+		processInput(window);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(0.2, 0.3, 0.4, 1);
 		float timeValue = glfwGetTime();
@@ -71,6 +144,13 @@ int main()
 		float Blue = abs(sin(timeValue * 1.5f));
 
 		glUseProgram(shader_program);
+
+		glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)WIN_WIDTH / (float)WIN_HEIGHT, 0.1f, 100.0f);
+		glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+		settingMat4(shader_program, "projection", projection);
+		settingMat4(shader_program, "view", view);
+
 		glBindVertexArray(VAO);
 		
 		uni4(shader_program, "ourColor", Red, Green, Blue, 1.0);
--- First_laba/assets/shader/vert_shader.vs
+++ First_laba/assets/shader/vert_shader.vs
@@ -1,6 +1,10 @@
 #version 410 core
 layout (location=0) in vec3 vp;
+uniform mat4 view;
+uniform mat4 projection;
+uniform mat4 model;
+uniform mat4 transform;
 void main() 
 {
-	gl_Position = vec4(vp, 1.0);
+	gl_Position = projection * view * vec4(vp, 1.0);
 }
